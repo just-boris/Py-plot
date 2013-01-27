0476818328d17f77069245c85c5e23daa21e1df1
Boris <just-boris@hotmail.com>
2013-01-27 18:36:28 +0400

    обработка кликов на график

--- heatmap.py
+++ heatmap.py
@@ -15,10 +15,22 @@ def intersect(a, b):
     def expr(x,y):
         return min(planar.gauss(x,y), cylinder.gauss(x,y))
     return integrate.dblquad(expr, xmin, xmax, lambda x: ymin, lambda x: ymax)[0]
+
+def drawMap(a, b):
+    ratio = intersect(a,b)
+    print ratio
+    return [[ratio*planar.gauss(x, y) for x in range(xmin, xmax+1)] for y in range(ymin, ymax+1)]
+
 vmax = planar.gauss(0,0)
-ratio = intersect(0,0)
-print ratio
-p = plt.imshow([[ratio*planar.gauss(x, y) for x in range(xmin, xmax)] for y in range(ymin, ymax)], extent=[xmin, xmax, ymin, ymax],vmax=vmax)
+p = plt.imshow(drawMap(0,0), extent=[xmin, xmax, ymin, ymax],vmax=vmax)
+point = plt.plot(0,0, 'b+')
+def OnClick(event):
+    p.set_data(drawMap(event.xdata, event.ydata))
+    point[0].set_data(event.xdata, event.ydata)
+    plt.show()
+fig = plt.gcf()
+plt.clim()
+cid_up = fig.canvas.mpl_connect('button_press_event', OnClick)
 plt.colorbar()
 plt.show()
 
