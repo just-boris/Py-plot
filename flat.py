c404f062f060c0db621b447c8bd42732ad182558
Boris <just-boris@hotmail.com>
2013-01-30 21:48:26 +0400

    heatmap - добавил таблицу с данными flat - заштриховал пересечение

--- flat.py
+++ flat.py
@@ -3,8 +3,8 @@ import scipy.integrate as integrate
 import numpy as np
 from gauss import GaussX
 
-planar = GaussX(4.5,0)
-
+planar = GaussX(7,-4)
+cylinder = GaussX(4.5,4)
 def substrat(x):
     cylinder = GaussX(4.5,x)
     def expr(x):
@@ -12,6 +12,9 @@ def substrat(x):
     return integrate.quad(expr, -20, 20)[0]
 
 x = np.arange(-20, 20, 0.1)
-
-pylab.plot(x, map(substrat, x), 'o')
+y1 = map(planar.gauss, x)
+y2 = map(cylinder.gauss, x)
+pylab.plot(x, y1, 'g')
+pylab.plot(x, y2, 'r')
+pylab.fill(x,np.minimum(y1,y2), hatch='\\', color='w')
 pylab.show()
--- heatmap.py
+++ heatmap.py
@@ -1,17 +1,27 @@
+# coding=utf-8
 import scipy.integrate as integrate
 import numpy as np
 from gauss import Gauss
-import matplotlib.pyplot as plt
+import pylab
+
+gridShape = (3,2)
+pylab.rc('font',**{'family':'verdana'})
 
 xmin = -20
 xmax = 20
 ymin = -20
 ymax = 20
 
-planar = Gauss(4.5, 4.5, 0, 0)
+planarG1 = 4.5
+planarG2 = 4.5
+cylinderG1 = 4.5
+cylinderG2 = 4.5
+planar = Gauss(planarG1, planarG1, 0, 0)
+vmax = planar.gauss(0,0)
 
+pylab.subplot2grid (gridShape, (0, 0), colspan = 2, rowspan = 2)
 def intersect(a, b):
-    cylinder = Gauss(2, 2, a, b)
+    cylinder = Gauss(cylinderG1, cylinderG2, a, b)
     def expr(x,y):
         return min(planar.gauss(x,y), cylinder.gauss(x,y))
     return integrate.dblquad(expr, xmin, xmax, lambda x: ymin, lambda x: ymax)[0]
@@ -19,18 +29,37 @@ def intersect(a, b):
 def drawMap(a, b):
     ratio = intersect(a,b)
     print ratio
-    return [[ratio*planar.gauss(x, y) for x in range(xmin, xmax+1)] for y in range(ymin, ymax+1)]
-
-vmax = planar.gauss(0,0)
-p = plt.imshow(drawMap(0,0), extent=[xmin, xmax, ymin, ymax],vmax=vmax)
-point = plt.plot(0,0, 'b+')
+    return [[ratio/vmax*planar.gauss(x, y) for x in range(xmin, xmax+1)] for y in range(ymin, ymax+1)]
+def getCellData(x,y):
+    return [
+        ["%s/%s"%(planarG1, planarG2)],
+        ["%s/%s"%(cylinderG1, cylinderG2)],
+        ["(%.4s, %.4s)"%(x, y)]
+    ]
+def buildTable(cells):
+    ax.table(
+        rowLabels=[u'Планарный', u'Цилиндрический', u'Точка пересечения'],
+        cellText=cells,
+        loc='center'
+    )
+p = pylab.imshow(drawMap(0,0), extent=[xmin, xmax, ymin, ymax])
+point = pylab.plot(0,0, 'b+')
 def OnClick(event):
-    p.set_data(drawMap(event.xdata, event.ydata))
-    point[0].set_data(event.xdata, event.ydata)
-    plt.show()
-fig = plt.gcf()
-plt.clim()
+    x = event.xdata
+    y = event.ydata
+    p.set_data(drawMap(x, y))
+    point[0].set_data(x, y)
+    buildTable(getCellData(x,y))
+    pylab.show()
+fig = pylab.gcf()
+
 cid_up = fig.canvas.mpl_connect('button_press_event', OnClick)
-plt.colorbar()
-plt.show()
+pylab.colorbar()
+
+ax = pylab.subplot2grid (gridShape, (2, 1), frame_on=False)
+ax.xaxis.set_visible(False)
+ax.yaxis.set_visible(False)
+buildTable(getCellData(0,0))
+pylab.show()
+
 
