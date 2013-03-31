034cd7dfed244408bc68fec50727f62dbeee8270
Boris <just-boris@hotmail.com>
2013-03-31 23:46:24 +0400

    оптимизировал подсчет двойного интеграла

--- include/coupling.py
+++ include/coupling.py
@@ -1,9 +1,17 @@
+import numpy as np
 import scipy.integrate as integrate
 
+def simps_dblquad(func, a, b, gfun, hfun):
+    def _infunc(x,func,gfun,hfun):
+        a = gfun(x)
+        b = hfun(x)
+        return integrate.simps([func(x, y) for y in np.arange(a, b, 0.1)],dx=0.1)
+    return integrate.simps([_infunc(x, func,gfun, hfun) for x in np.arange(a, b, 0.1)],dx=0.1)
+
 def coupling(input, out):
-    A = integrate.dblquad(lambda x,y: input(x,y)*out(x,y), -20, 20, lambda x: -20, lambda y: 20)[0]
-    B = integrate.dblquad(lambda x,y: input(x,y)**2, -20, 20, lambda x: -20, lambda y: 20)[0]
-    C = integrate.dblquad(lambda x,y: out(x,y)**2, -20, 20, lambda x: -20, lambda y: 20)[0]
+    A = simps_dblquad(lambda x,y: input(x,y)*out(x,y), -20, 20, lambda x: -20, lambda y: 20)
+    B = simps_dblquad(lambda x,y: input(x,y)**2, -20, 20, lambda x: -20, lambda y: 20)
+    C = simps_dblquad(lambda x,y: out(x,y)**2, -20, 20, lambda x: -20, lambda y: 20)
     return A*A/B/C
 
 def couplingX(input, out):
