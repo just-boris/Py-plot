b4372bbde26206d5a7440d6977056c87bfdde92c
Boris <just-boris@hotmail.com>
2013-04-10 23:50:10 +0400

    добавил демку для просмотра планара

---
+++ demo_planar.py
@@ -0,0 +1,28 @@
+import numpy as np
+from include.plot3D import doPlot
+from planar import Planar
+import pylab
+
+xmin = -7
+xmax = 7
+ymin = -7
+ymax = 7
+
+use3d = False
+
+planar = Planar(open('matrix/dump2d.csv', 'rb'))
+
+def drawMap():
+    return [[planar.func(x, y) for x in range(xmin, xmax+1)] for y in range(ymin, ymax+1)]
+
+x = np.arange(xmin, xmax+1, 1)
+y = np.arange(ymin, ymax+1, 1)
+
+if use3d:
+    doPlot(x, y, lambda x,y: planar.func(x,y))
+else:
+    CS = pylab.contour(x, y, drawMap(), 10, colors='k')
+    pylab.clabel(CS, fontsize=9, inline=1)
+    pylab.show()
+
+
